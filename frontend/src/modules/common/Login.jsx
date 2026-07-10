bc6ee51e09c3b43a71ece13c18b362a81352de16
karrisribhagyadhaneswarareddy-maker <karrisribhagyadhaneswarareddy@gmail.com>
2026-07-10 12:24:50 +0530

    Updated backend URL for deployment

--- frontend/src/modules/common/Login.jsx
+++ frontend/src/modules/common/Login.jsx
@@ -1,25 +1,25 @@
-import React, { useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom';
-import Navbar from 'react-bootstrap/Navbar';
-import { Container, Nav } from 'react-bootstrap';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import axios from 'axios';
-import { message } from 'antd';
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import Navbar from "react-bootstrap/Navbar";
+import { Container, Nav } from "react-bootstrap";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import axios from "axios";
+import { message } from "antd";
 
 const Login = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const [data, setData] = useState({
     name: "",
     email: "",
     password: "",
-    type: ""
-  })
+    type: "",
+  });
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -32,7 +32,11 @@ const Login = () => {
     if (!data?.email || !data?.password) {
       return alert("Please fill all fields");
     } else {
-      axios.post('http://localhost:8001/api/user/login', data)
+      axios
+        .post(
+          "https://house-rent-backend-r585.onrender.com/api/user/login",
+          data,
+        )
         .then((res) => {
           if (res.data.success) {
             message.success(res.data.message);
@@ -49,8 +53,10 @@ const Login = () => {
                 navigate("/renterhome");
                 break;
               case "Owner":
-                if (isLoggedIn.granted === 'ungranted') {
-                  message.error('Your account is not yet confirmed by the admin');
+                if (isLoggedIn.granted === "ungranted") {
+                  message.error(
+                    "Your account is not yet confirmed by the admin",
+                  );
                 } else {
                   navigate("/ownerhome");
                 }
@@ -59,9 +65,9 @@ const Login = () => {
                 navigate("/login");
                 break;
             }
-            setTimeout(()=>{
-              window.location.reload()
-            },1000)
+            setTimeout(() => {
+              window.location.reload();
+            }, 1000);
           } else {
             message.error(res.data.message);
           }
@@ -79,44 +85,42 @@ const Login = () => {
     <>
       <Navbar expand="lg" className="bg-body-tertiary">
         <Container fluid>
-          <Navbar.Brand><h2>RentEase</h2></Navbar.Brand>
+          <Navbar.Brand>
+            <h2>RentEase</h2>
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="navbarScroll" />
           <Navbar.Collapse id="navbarScroll">
             <Nav
               className="me-auto my-2 my-lg-0"
-              style={{ maxHeight: '100px' }}
+              style={{ maxHeight: "100px" }}
               navbarScroll
-            >
-            </Nav>
+            ></Nav>
             <Nav>
-              <Link to={'/'}>Home</Link>
-              <Link to={'/login'}>Login</Link>
-              <Link to={'/register'}>Register</Link>
+              <Link to={"/"}>Home</Link>
+              <Link to={"/login"}>Login</Link>
+              <Link to={"/register"}>Register</Link>
             </Nav>
-
           </Navbar.Collapse>
         </Container>
       </Navbar>
 
-
-      <Container component="main" >
+      <Container component="main">
         <Box
           sx={{
             marginTop: 8,
             marginBottom: 4,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
           }}
         >
-          <Avatar sx={{ bgcolor: 'secondary.main' }}>
+          <Avatar sx={{ bgcolor: "secondary.main" }}>
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
             Sign In
           </Typography>
           <Box component="form" onSubmit={handleSubmit} noValidate>
-
             <TextField
               margin="normal"
               fullWidth
@@ -143,19 +147,29 @@ const Login = () => {
               <Button
                 type="submit"
                 variant="contained"
-                style={{ width: '200px' }}
+                style={{ width: "200px" }}
               >
                 Sign Up
               </Button>
             </Box>
             <Grid container>
-              <Grid item>forgot password?
-                <Link style={{ color: "red" }} to={'/forgotpassword'} variant="body2">
+              <Grid item>
+                forgot password?
+                <Link
+                  style={{ color: "red" }}
+                  to={"/forgotpassword"}
+                  variant="body2"
+                >
                   {" Click here"}
                 </Link>
               </Grid>
-              <Grid item>Have an account?
-                <Link style={{ color: "blue" }} to={'/register'} variant="body2">
+              <Grid item>
+                Have an account?
+                <Link
+                  style={{ color: "blue" }}
+                  to={"/register"}
+                  variant="body2"
+                >
                   {" Sign Up"}
                 </Link>
               </Grid>
@@ -164,7 +178,7 @@ const Login = () => {
         </Box>
       </Container>
     </>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- frontend/src/modules/common/Register.jsx
+++ frontend/src/modules/common/Register.jsx
@@ -1,27 +1,27 @@
-import React, { useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom';
-import Navbar from 'react-bootstrap/Navbar';
-import { Container, Nav } from 'react-bootstrap';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import Select from '@mui/material/Select';
-import axios from 'axios';
-import { message } from 'antd';
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import Navbar from "react-bootstrap/Navbar";
+import { Container, Nav } from "react-bootstrap";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import Select from "@mui/material/Select";
+import axios from "axios";
+import { message } from "antd";
 const Register = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const [data, setData] = useState({
     name: "",
     email: "",
     password: "",
-    type: ""
-  })
+    type: "",
+  });
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -29,17 +29,21 @@ const Register = () => {
   };
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-    if (!data?.name || !data?.email || !data?.password||! data?.type ) return alert("Please fill all fields");
+    e.preventDefault();
+    if (!data?.name || !data?.email || !data?.password || !data?.type)
+      return alert("Please fill all fields");
     else {
-      axios.post('http://localhost:8001/api/user/register', data)
+      axios
+        .post(
+          "https://house-rent-backend-r585.onrender.com/api/user/register",
+          data,
+        )
         .then((response) => {
           if (response.data.success) {
             message.success(response.data.message);
-            navigate('/login')
-
+            navigate("/login");
           } else {
-            message.error(response.data.message)
+            message.error(response.data.message);
           }
         })
         .catch((error) => {
@@ -48,41 +52,40 @@ const Register = () => {
     }
   };
 
-
   return (
     <>
       <Navbar expand="lg" className="bg-body-tertiary">
         <Container fluid>
-          <Navbar.Brand><h2>RentEase</h2></Navbar.Brand>
+          <Navbar.Brand>
+            <h2>RentEase</h2>
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="navbarScroll" />
           <Navbar.Collapse id="navbarScroll">
             <Nav
               className="me-auto my-2 my-lg-0"
-              style={{ maxHeight: '100px' }}
+              style={{ maxHeight: "100px" }}
               navbarScroll
-            >
-            </Nav>
+            ></Nav>
             <Nav>
-              <Link to={'/'}>Home</Link>
-              <Link to={'/login'}>Login</Link>
-              <Link to={'/register'}>Register</Link>
+              <Link to={"/"}>Home</Link>
+              <Link to={"/login"}>Login</Link>
+              <Link to={"/register"}>Register</Link>
             </Nav>
-
           </Navbar.Collapse>
         </Container>
       </Navbar>
 
-      <Container component="main" >
+      <Container component="main">
         <Box
           sx={{
             marginTop: 8,
             marginBottom: 4,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
           }}
         >
-          <Avatar sx={{ bgcolor: 'secondary.main' }}>
+          <Avatar sx={{ bgcolor: "secondary.main" }}>
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
@@ -126,29 +129,32 @@ const Register = () => {
             <Select
               labelId="demo-simple-select-label"
               id="demo-simple-select"
-              name='type'
+              name="type"
               value={data.type}
               label="type"
               defaultValue="Select User"
               onChange={handleChange}
-              style={{ width: '200px' }}
+              style={{ width: "200px" }}
             >
-              <MenuItem value={'Select User'} disabled>Select User</MenuItem>
-              <MenuItem value={'Renter'}>Renter</MenuItem>
+              <MenuItem value={"Select User"} disabled>
+                Select User
+              </MenuItem>
+              <MenuItem value={"Renter"}>Renter</MenuItem>
               <MenuItem value={"Owner"}>Owner</MenuItem>
             </Select>
             <Box mt={2}>
               <Button
                 type="submit"
                 variant="contained"
-                style={{ width: '200px' }}
+                style={{ width: "200px" }}
               >
                 Sign Up
               </Button>
             </Box>
             <Grid container>
-              <Grid item>Have an account?
-                <Link style={{ color: "blue" }} to={'/login'} variant="body2">
+              <Grid item>
+                Have an account?
+                <Link style={{ color: "blue" }} to={"/login"} variant="body2">
                   {" Sign In"}
                 </Link>
               </Grid>
@@ -157,7 +163,7 @@ const Register = () => {
         </Box>
       </Container>
     </>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
